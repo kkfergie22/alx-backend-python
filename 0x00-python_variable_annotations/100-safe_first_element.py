181d3b9bb58ac5c1ffad12d72d62f30c14793f29
Christian Koranteng <korantengchristian@gmail.com>
2023-04-15 16:45:20 +0000

    Fix 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,10 +3,10 @@
 """This module contains a function that returns the first element of a list"""
 
 # The types of the elements of the input are not known
-from typing import List, Any, Optional
+from typing import Any, Optional, Sequence, Union
 
 
-def safe_first_element(lst: List[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Union[Any, None]]:
     """This function returns the first element of a list"""
     if lst:
         return lst[0]
