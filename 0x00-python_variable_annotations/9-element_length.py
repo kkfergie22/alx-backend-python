1ca927864e224e4a4351507edde1dea69acbf362
Christian Koranteng <korantengchristian@gmail.com>
2023-04-15 16:38:43 +0000

    Fix 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,18 +2,18 @@
 
 """This function returns a list of tuples containing
 each element of the input list"""
-from typing import List, Tuple, Any
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[Any]) -> List[Tuple[Any, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of tuples containing each element of the input list
     and its length.
 
     Args:
-        lst (List[Any]): The list to process.
+      The list to process.
 
     Returns:
-        List[Tuple[Any, int]]: The list of tuples containing each element
+      The list of tuples containing each element
         of the input list and its length.
     """
     return [(i, len(i)) for i in lst]
