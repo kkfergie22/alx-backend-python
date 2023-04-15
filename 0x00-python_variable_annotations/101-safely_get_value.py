a0412d137f5ca5321f7de3f0e83ac927ad29041b
Christian Koranteng <korantengchristian@gmail.com>
2023-04-15 17:05:49 +0000

    Fix 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from typing import Dict, Union, TypeVar
+from typing import Mapping, Union, TypeVar
 
 K = TypeVar('K')
 V = TypeVar('V')
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Dict[K, V], key: K, default:
+def safely_get_value(dct: Mapping[K, V], key: K, default:
                      Union[T, None] = None) -> Union[V, T]:
     """Returns the value associated with the given key in a dictionary, or
     a default value if the key is not present in the dictionary.
