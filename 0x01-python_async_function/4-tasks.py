eec15e1afb710ad4d9f31af321982ade4a1e3c6a
Christian Koranteng <korantengchristian@gmail.com>
2023-04-19 12:22:27 +0000

    Upload 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" This module creates an asyncio.Task object from task_wait_random """
+
+import asyncio
+from typing import List
+from tasks import task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random n times with the specified max_delay.
+    Returns a list of the delays (float values) in ascending order.
+    """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return sorted(delays)
