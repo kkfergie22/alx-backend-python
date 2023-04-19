77c630fb5f9598f86caa8cbbd3fd58e10a3cd18f
Christian Koranteng <korantengchristian@gmail.com>
2023-04-19 17:16:55 +0000

    Upload task 2

--- 0x02-python_async_comprehension/test.py
+++ 0x02-python_async_comprehension/test.py
@@ -2,10 +2,13 @@
 
 import asyncio
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def main():
-    print(await async_comprehension())
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
 
-asyncio.run(main())
+asyncio.run(print_yielded_values())
