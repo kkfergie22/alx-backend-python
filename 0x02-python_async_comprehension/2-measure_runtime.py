15e42626bf7d49dc627f2b950c4eadcfa0c61465
Christian Koranteng <korantengchristian@gmail.com>
2023-04-19 17:08:06 +0000

    Upload task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,8 +11,7 @@ async def measure_runtime() -> float:
     running in parallel using asyncio.gather.
     """
     start_time = time.perf_counter()
-    await asyncio.gather(
-        *[asyncio.create_task(async_comprehension()) for _ in range(4)]
-    )
+    new_tasks = [asyncio.create_task(async_comprehension()) for i in range(4)]
+    await asyncio.gather(*new_tasks)
     total_time = time.perf_counter() - start_time
     return total_time
