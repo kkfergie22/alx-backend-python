a923cd5e6f1aca7ef13a6d56797a03577de5b901
Christian Koranteng <korantengchristian@gmail.com>
2023-04-25 09:10:59 +0000

    test docs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,51 +12,51 @@ from utils import access_nested_map
 from utils import get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test case class for access_nested_map function"""
+# class TestAccessNestedMap(unittest.TestCase):
+#     """Test case class for access_nested_map function"""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(
-        self,
-        nested_map: Mapping,
-        path: Sequence,
-        expected_output: Any
-    ) -> None:
-        """Test for access_nested_map function"""
-        self.assertEqual(access_nested_map(nested_map, path), expected_output)
+#     @parameterized.expand([
+#         ({"a": 1}, ("a",), 1),
+#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#         ({"a": {"b": 2}}, ("a", "b"), 2),
+#     ])
+#     def test_access_nested_map(
+#         self,
+#         nested_map: Mapping,
+#         path: Sequence,
+#         expected_output: Any
+#     ) -> None:
+#         """Test for access_nested_map function"""
+#         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
-    @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """
-        Test that access_nested_map raises a KeyError
-        when the given key is not found in the nested map.
-        """
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_error_message)
+#     @parameterized.expand([
+#         ({}, ("a",), "a"),
+#         ({"a": 1}, ("a", "b"), "b"),
+#     ])
+#     def test_access_nested_map_exception(self, nested_map, path):
+#         """
+#         Test that access_nested_map raises a KeyError
+#         when the given key is not found in the nested map.
+#         """
+#         with self.assertRaises(KeyError) as context:
+#             access_nested_map(nested_map, path)
+#         self.assertEqual(str(context.exception), expected_error_message)
 
 
-class TestGetJson(unittest.TestCase):
-    @patch('requests.get')
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, test_url, test_payload, mock_get):
-        mock_json = Mock(return_value=test_payload)
-        mock_get.return_value = Mock(json=mock_json)
+# class TestGetJson(unittest.TestCase):
+#     @patch('requests.get')
+#     @parameterized.expand([
+#         ("http://example.com", {"payload": True}),
+#         ("http://holberton.io", {"payload": False})
+#     ])
+#     def test_get_json(self, test_url, test_payload, mock_get):
+#         mock_json = Mock(return_value=test_payload)
+#         mock_get.return_value = Mock(json=mock_json)
 
-        result = get_json(test_url)
+#         result = get_json(test_url)
 
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
+#         mock_get.assert_called_once_with(test_url)
+#         self.assertEqual(result, test_payload)
 
 
 class TestMemoize(unittest.TestCase):
